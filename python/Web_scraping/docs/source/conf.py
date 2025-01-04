fd62d890b75682f4626e97cd1fbecefc627451e4
igapon <igapon@gmail.com>
2025-01-04 10:27:57 +0900

    conf.py修正

--- python/Web_scraping/docs/source/conf.py
+++ python/Web_scraping/docs/source/conf.py
@@ -7,17 +7,20 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
-
+from recommonmark.parser import CommonMarkParser
+source_suffix = ['.rst', '.md']
+source_parsers = {
+    '.md': CommonMarkParser,
+}
 sys.path.insert(0, os.path.abspath('../../'))
 # print(sys.path)
 project = 'Web_scraping'
-copyright = '2024, Ryosuke Igarashi'
+copyright = '2025, Ryosuke Igarashi'
 author = 'Ryosuke Igarashi'
 release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
@@ -25,12 +28,10 @@ extensions = [
 ]
 templates_path = ['_templates']
 exclude_patterns = []
-
 language = 'ja'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 # html_theme = 'sphinx_material'
