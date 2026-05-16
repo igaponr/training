b43582f95b4195de7e6bcfde869f42f9d25669ea
igaponr <igapon@gmail.com>
2026-05-16 09:56:23 +0900

    言語選択

--- python/Web_scraping/crawling_template.py
+++ python/Web_scraping/crawling_template.py
@@ -67,7 +67,7 @@ page_selectors = {
                   lambda el: el.text),
                  ],
     'languages': [(By.XPATH,
-                   '//section[@id="tags"]//a[contains(@href, "/language/")]/span[@class="name"]',
+                   '//section[@id="tags"]//a[contains(@href, "/language/")]/span[contains(@class, "name")]',
                    lambda el: el.text),
                   ],
 }
